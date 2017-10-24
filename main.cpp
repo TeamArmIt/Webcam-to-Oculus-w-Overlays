6f0a12fa79aaf07086768cdd28ddad53581d5b32
Sai <ksaimouli@gmail.com>
2017-10-24 17:25:47 -0400

    -with gimbal code (need to test)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,59 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2017, STEREOLABS.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
+
+/***********************************************************************************************
+** AUTHOR: Saimouli Katragadda 2018
+
+** This code converts oculus roataions into quat and stores into a file					  	  **
+
+
+***********************************************************************************************/
+
 #include <OVR_CAPI.h>
 #include <iostream>
+#include "Extras/OVR_Math.h"
+#include "OVR_CAPI_D3D.h"
 #include <thread>
 #include <fstream>
 
-using namespace std;
+#include <Windows.h>
+#include <string>
+#include <vector>
 
-void HeadTrackingApplication(void);
+#define PI 3.14159
+
+using namespace std;
+float yaw, pitch, roll, speed;
+float rSpeed, pSpeed, ySpeed;
+void HeadTrackingApplication();
 
 int main(void) {
-	cout << "running app" << endl;
+	
+
 	HeadTrackingApplication();
 		
 	system("pause");
 	return 0;
 }
  
-void HeadTrackingApplication(void){
+void HeadTrackingApplication(){
 	ofstream myfile;
 	myfile.open("headtrackingdata.txt");
 
@@ -33,6 +71,14 @@ void HeadTrackingApplication(void){
 	ovrHmdDesc desc = ovr_GetHmdDesc(session);
 	ovrSizei resolution = desc.Resolution;
 
+	ovrTrackingState ts;
+	ts = ovr_GetTrackingState(session, ovr_GetTimeInSeconds(), ovrTrue);
+	//cout << "running app" << endl;
+	ovrPosef pose = ts.HeadPose.ThePose;
+	ovrQuatf orient = pose.Orientation;
+
+	OVR::Quatf Oculus = pose.Orientation;
+	Oculus.GetEulerAngles <OVR::Axis_X, OVR::Axis_Y, OVR::Axis_Z>(&pitch, &yaw, &roll);
 	// orientation data
 	while (true) {
 		ovrTrackingState ts;
@@ -41,21 +87,37 @@ void HeadTrackingApplication(void){
 		if (ts.StatusFlags & (ovrStatus_OrientationTracked | ovrStatus_PositionTracked)){
 			ovrPosef pose = ts.HeadPose.ThePose;
 			ovrQuatf orient = pose.Orientation;
+			OVR::Quatf Oculus = pose.Orientation;
+			//Oculus.GetYawPitchpitch;
+			Oculus.GetEulerAngles <OVR::Axis_X, OVR::Axis_Y, OVR::Axis_Z> (&pitch,&yaw,&roll);
+			pitch = pitch*(180 / PI);
+			yaw = yaw*(180 / PI);
+			roll = roll*(180 / PI);
+			//converting rad to degrees
 
-			cout << "Orientation data:" <<endl;
-			cout << "x: " << orient.x << endl;
-			cout << "y: " << orient.y << endl;
-			cout << "z: " << orient.z << endl;
+			//cout << "Orientation data:" << endl;
+			cout << "pitch " << pitch << "yaw: "<< yaw<<" roll:"<< roll <<endl;
+			
+			//cout << "z: " << orient.z << endl;
 
 			//writing to the file 
-			myfile << orient.x << endl;
-			myfile << orient.y << endl;
-			myfile << orient.z << endl;
-			myfile << endl;
+			//myfile << orient.x << endl;
 
-			this_thread::sleep_for(std::chrono::milliseconds(100));
+			rSpeed = roll / 0.01;// to match the speed of the gimbal with the rate of oculus headtracking data 
+			pSpeed = pitch / 0.01;
+			ySpeed = yaw / 0.01;
+			myfile << "A RA("<<roll<<") RS("<< rSpeed <<")" << endl;
+			myfile << "A PA(" << pitch << ") PS(" << pSpeed << ")" << endl;
+			myfile << "A YA("<<yaw << ") YS("<< ySpeed << ")" << endl;
+
+			this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	}
 	ovr_Destroy(session);
 	ovr_Shutdown();
 }
+
+// 1st value to array 
+// 2nd value-1st value  //index 0
+// 3rd-2nd value //index 1
+
